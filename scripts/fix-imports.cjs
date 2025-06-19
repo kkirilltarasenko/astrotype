b4d97ed79b8a788d36b84d52a1d51f497fdb4d83
Kirill <k.tarasenko@atomgroup.io>
2025-06-20 01:00:09 +0300

    feat(CLI-2): add project build, start, and postscript flow, missing deps and root file

---
+++ scripts/fix-imports.cjs
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const DIST_DIR = path.join(__dirname, '../dist');
+
+function fixImportsInFile(filePath) {
+    let content = fs.readFileSync(filePath, 'utf8');
+    content = content.replace(/(from\s+['"])(\.\.?\/[^'".]+)(['"])/g, (match, p1, importPath, p3) => {
+        const absPath = path.resolve(path.dirname(filePath), importPath);
+        if (fs.existsSync(absPath + '.js')) {
+            return `${p1}${importPath}.js${p3}`;
+        } else if (fs.existsSync(path.join(absPath, 'index.js'))) {
+            return `${p1}${importPath}/index.js${p3}`;
+        } else {
+            return match;
+        }
+    });
+    fs.writeFileSync(filePath, content, 'utf8');
+}
+
+function walkDir(dir) {
+    fs.readdirSync(dir).forEach(file => {
+        const fullPath = path.join(dir, file);
+        if (fs.statSync(fullPath).isDirectory()) {
+            walkDir(fullPath);
+        } else if (file.endsWith('.js')) {
+            fixImportsInFile(fullPath);
+        }
+    });
+}
+
+walkDir(DIST_DIR); 
