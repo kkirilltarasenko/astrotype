e2fe95c41b68e556cb4a46992ef421aa8073a0f3
Kirill Tarasenko <Kirill.Tarasenko@broit.io>
2025-07-17 13:53:29 +0300

    fix(CLI-12): add abs path to env file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 import dotenv from 'dotenv';
+import path from 'path';
 
 import Root from '@/core/root';
 
 (async () => {
-  dotenv.config();
+  dotenv.config({ path: path.resolve(__dirname, '../.env') });
   await Root.init();
 })();
