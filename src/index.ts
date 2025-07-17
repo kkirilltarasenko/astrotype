6f57a1d77b909ee1d21f23b5959095717a1efc37
Kirill Tarasenko <Kirill.Tarasenko@broit.io>
2025-07-17 13:54:25 +0300

    fix(CLI-12): fix esm dirname path

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@
 
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 import Root from '@/core/root';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 (async () => {
   dotenv.config({ path: path.resolve(__dirname, '../.env') });
